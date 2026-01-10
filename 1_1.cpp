767800918d4c98fc5d78018ed666fbe8008f4a44
Anirudh <anirudh.r22@proton.me>
2026-01-10 16:29:27 +0000

    First program

---
+++ 1_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int perf(int a){
+	int sum = 0;
+	for(int i = 1;i < a;i++){
+		if (a % i == 0){
+			sum += i;
+		}
+	}
+	return sum;
+}
+
+int main(){
+	int a;
+	cout << "Enter the number to check if its a perfect number: ";
+	cin >> a;
+	int b = perf(a);
+	if(a == b){
+		cout << "The number " << a << " is a perfect number" << endl;
+	}else{
+		cout << "The number " << a << " is not a perfect number" << endl;
+	}
+	return 0;
+}
