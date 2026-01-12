db0da583f47c7da57419ac51bc4aa750b44635e9
Shawarma <74491153+Shawarma22@users.noreply.github.com>
2026-01-12 10:42:03 +0530

    program p4 added

--- No.1/p4.cpp
+++ No.1/p4.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-float num(){
-
-}
-
-float denm(){
-
-}
-
-float sin(){
-
+int factc(int n){
+	int fact;
+	for(int i = 1; i <= n; i++){
+		fact *= i;
+	}return fact;
 }
 
 int main(){
-
+	int x, n, iter = 1, i = 1, sign = 1;
+	double sin = 0.0;
+	cout << "Enter x value in sin(x) and total number of terms: ";
+	cin >> x >> n;
+	cout << "Caclulating sin(" << x << ") value with " << n << " terms...\n";
+	while(iter < n){
+		if(iter % 2 == 0){
+			sign = -1;
+		}else{
+			sign = 1;
+		}
+		sin += sign * (pow(x, i) / factc(i));
+		iter++;
+		i += 2;
+	}cout << "Approximate sin(" << x << ") value is: " << sin << endl;
+	return 0;
 }
