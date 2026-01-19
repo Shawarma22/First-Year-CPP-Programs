bdc86c924a1300140638c0833e4862a2e5eaf985
Shawarma <74491153+Shawarma22@users.noreply.github.com>
2026-01-19 09:01:21 +0530

    Update p4.cpp

--- No.1/p4.cpp
+++ No.1/p4.cpp
@@ -2,26 +2,22 @@
 #include <cmath>
 using namespace std;
 
-int factc(int n){
-	int fact;
+double factc(int n){
+	double fact = 1;
 	for(int i = 1; i <= n; i++){
 		fact *= i;
 	}return fact;
 }
 
 int main(){
-	int x, n, iter = 1, i = 1, sign = 1;
-	double sin = 0.0;
+	int n, iter = 0, i = 1, sign = 1;
+	double x, sin = 0.0;
 	cout << "Enter x value in sin(x) and total number of terms: ";
 	cin >> x >> n;
 	cout << "Caclulating sin(" << x << ") value with " << n << " terms...\n";
 	while(iter < n){
-		if(iter % 2 == 0){
-			sign = -1;
-		}else{
-			sign = 1;
-		}
 		sin += sign * (pow(x, i) / factc(i));
+		sign *= -1;
 		iter++;
 		i += 2;
 	}cout << "Approximate sin(" << x << ") value is: " << sin << endl;
