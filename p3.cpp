11602ec6c7cc6fc58f4f11764df2e4837bd6372e
Shawarma <74491153+Shawarma22@users.noreply.github.com>
2026-01-31 10:38:44 +0530

    p3 started e

---
+++ p3.cpp
@@ -0,0 +1,13 @@
+//Anirudh Ramasubramanian --> 129157003 --> K1 section --> B.Tech. CSE (CS & BT)
+//https://github.com/Shawarma22
+
+#include <iostream>
+using namespace std;
+
+void getInput(int &x, int &y){
+	cout << "Enter the values of the base and exponent: ";
+	cin >> x >> y;
+}
+
+void calculatePower(int &x, int &y){
+}
